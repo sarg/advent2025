ee0b401665036d6bc516499f2d0187e0eac6abc2
Sergey Trofimov <sarg@sarg.org.ru>
2025-12-07 16:08:50 +0100

    day 7 simplify

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -7,31 +7,22 @@ fn main() -> std::io::Result<()> {
     let mut lines_iter = BufReader::new(file).lines();
 
     let start = lines_iter.next().unwrap().unwrap();
-    let s_idx = start.find('S').expect("Start must be there");
+    let mut row = vec![0 as u64; start.len()];
+    row[start.find('S').expect("Start must be there")] = 1;
 
-    let mut row = start.into_bytes();
-    row[s_idx] = b'|';
-
-    let mut reachable: Vec<u64> = vec![0; row.len()];
-    reachable[s_idx] = 1;
     let mut splits = 0;
     for line in lines_iter {
         line?.chars().enumerate().for_each(|(i, c)| {
-            if c == '^' && row[i] == b'|' {
+            if c == '^' && row[i] > 0 {
                 splits += 1;
-
-                reachable[i - 1] += reachable[i];
-                reachable[i + 1] += reachable[i];
-                reachable[i] = 0;
-
-                row[i] = b'.';
-                row[i - 1] = b'|';
-                row[i + 1] = b'|';
+                row[i - 1] += row[i];
+                row[i + 1] += row[i];
+                row[i] = 0;
             }
         });
     }
     println!("Part 1: {splits}");
-    println!("Part 2: {}", reachable.iter().sum::<u64>());
+    println!("Part 2: {}", row.iter().sum::<u64>());
 
     Ok(())
 }
