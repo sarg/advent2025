08c1155f9d36058a7b0a56bdddc8e00317630728
Sergey Trofimov <sarg@sarg.org.ru>
2025-12-08 12:28:27 +0100

    day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -7,11 +7,24 @@ use std::io::BufReader;
 
 type Point = (i64, i64, i64);
 
-fn dist(p1: &Point, p2: &Point) -> u64 {
-    let d2 = (p1.0 - p2.0) * (p1.0 - p2.0)
-        + (p1.1 - p2.1) * (p1.1 - p2.1)
-        + (p1.2 - p2.2) * (p1.2 - p2.2);
-    (d2 as u64).isqrt()
+fn dist(p1: &Point, p2: &Point) -> i64 {
+    (p1.0 - p2.0).pow(2) + (p1.1 - p2.1).pow(2) + (p1.2 - p2.2).pow(2)
+}
+
+fn union(connects: &mut Vec<usize>, sizes: &mut HashMap<usize, usize>, p: (usize, usize)) {
+    let r1 = connects[p.0];
+    let r2 = connects[p.1];
+    if r1 == r2 {
+        return;
+    }
+    let s2 = sizes.remove(&r2).unwrap();
+    let s1 = sizes.remove(&r1).unwrap();
+    sizes.insert(r1, s1 + s2);
+    for i in 0..connects.len() {
+        if connects[i] == r2 {
+            connects[i] = r1;
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -29,27 +42,25 @@ fn main() -> std::io::Result<()> {
         sizes.insert(i, 1);
     }
 
-    (0..points.len())
+    let mut pairs = (0..points.len())
         .tuple_combinations()
-        .sorted_unstable_by_key(|&(p1, p2)| dist(&points[p1], &points[p2]))
-        .take(1000)
-        .for_each(|(p1, p2)| {
-            let r1 = connects[p1];
-            let r2 = connects[p2];
-            if r1 != r2 {
-                let s2 = sizes.remove(&r2).unwrap();
-                let s1 = sizes.remove(&r1).unwrap();
-                sizes.insert(r1, s1 + s2);
-                for i in 0..connects.len() {
-                    if connects[i] == r2 {
-                        connects[i] = r1;
-                    }
-                }
-            }
-        });
+        .sorted_unstable_by_key(|&(p1, p2)| dist(&points[p1], &points[p2]));
 
+    for p in pairs.by_ref().take(1000) {
+        union(&mut connects, &mut sizes, p);
+    }
     let part1 = sizes.values().sorted().rev().take(3).product::<usize>();
+
+    let last = pairs
+        .find(|&p| {
+            union(&mut connects, &mut sizes, p);
+            sizes.len() == 1
+        })
+        .unwrap();
+
+    let part2 = points[last.0].0 * points[last.1].0;
     println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 
     Ok(())
 }
