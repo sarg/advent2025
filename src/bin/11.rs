7d5b64fce7c7731e48f934a54197243011bc267d
Sergey Trofimov <sarg@sarg.org.ru>
2025-12-11 10:54:04 +0100

    day 11 part 2 simplify

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -3,89 +3,57 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-struct Graph {
-    vertices: BTreeMap<String, usize>,
-    edges: Vec<Vec<usize>>,
+type Graph<'a> = BTreeMap<&'a str, Vec<&'a str>>;
+
+fn _path_count<'a>(
+    g: &'a Graph,
+    from: &'a str,
+    to: &'a str,
+    cache: &mut BTreeMap<&'a str, usize>,
+) -> usize {
+    if from == to {
+        1
+    } else if let Some(&n) = cache.get(from) {
+        n
+    } else if let Some(adjacent) = g.get(from) {
+        let n = adjacent.iter().map(|v| _path_count(g, v, to, cache)).sum();
+        cache.insert(from, n);
+        n
+    } else {
+        0
+    }
 }
 
-impl Graph {
-    fn new() -> Self {
-        Self {
-            vertices: BTreeMap::new(),
-            edges: Vec::new(),
-        }
-    }
-
-    fn idx(&mut self, v: String) -> usize {
-        match self.vertices.get(&v) {
-            None => {
-                let idx = self.vertices.len();
-                self.vertices.insert(v, idx);
-                self.edges.push(vec![]);
-                idx
-            }
-            Some(&v) => v,
-        }
-    }
-
-    fn add_edge(&mut self, from: String, to: String) {
-        let f = self.idx(from);
-        let t = self.idx(to);
-        self.edges[f].push(t);
-    }
-
-    fn _path_count(&self, from: usize, to: usize, cache: &mut BTreeMap<usize, usize>) -> usize {
-        if let Some(&n) = cache.get(&from) {
-            return n;
-        }
-
-        let count = if from == to {
-            1
-        } else {
-            self.edges[from]
-                .iter()
-                .map(|&v| self._path_count(v, to, cache))
-                .sum()
-        };
-
-        cache.insert(from, count);
-        count
-    }
-
-    fn _idx(&self, v: &str) -> usize {
-        *self.vertices.get(&v.to_string()).expect("must exist")
-    }
-
-    fn path_count(&self, path: &[&str]) -> usize {
-        path.iter()
-            .zip(path.iter().skip(1))
-            .map(|(a, b)| self._path_count(self._idx(a), self._idx(b), &mut BTreeMap::new()))
-            .product()
-    }
+fn path_count(g: &Graph, path: &[&str]) -> usize {
+    path.iter()
+        .zip(path.iter().skip(1))
+        .map(|(&a, &b)| _path_count(g, a, b, &mut BTreeMap::new()))
+        .product()
 }
 
 fn main() -> std::io::Result<()> {
     let file = File::open("input/11")?;
     let mut graph = Graph::new();
 
-    let reader = BufReader::new(file);
-    for l in reader.lines() {
-        let line = l?;
+    let lines: Vec<String> = BufReader::new(file)
+        .lines()
+        .filter_map(Result::ok)
+        .collect();
 
+    for line in lines.iter() {
         let mut liter = line.split(' ');
         let mut from = liter.next().unwrap();
         from = &from[0..from.len() - 1];
-
-        for to in liter {
-            graph.add_edge(from.to_string(), to.to_string());
-        }
+        graph.insert(from, liter.collect());
     }
 
-    let part1 = graph.path_count(&["you", "out"]);
+    let part1 = path_count(&graph, &["you", "out"]);
     println!("Part 1: {part1}");
 
-    let part2 = graph.path_count(&["svr", "dac", "fft", "out"])
-        + graph.path_count(&["svr", "fft", "dac", "out"]);
+    let part2: usize = [["svr", "dac", "fft", "out"], ["svr", "fft", "dac", "out"]]
+        .iter()
+        .map(|p| path_count(&graph, p))
+        .sum();
 
     println!("Part 2: {part2}");
 
